e9ca5d42506db2b9cc46353e573adeb6a08f21b7
tonywarwick69 <duc.khang165@gmail.com>
2022-10-06 10:47:38 +0700

    b6bai1

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import Add from './components/Add';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Items from './components/Items';
 
 export default function App() {
+
+  const [initialElements, changeEl]  = useState([
+   
+  ]);
+
+  const [exampleState, setExampleState] = useState(initialElements);
+  const [idx, incr] = useState(0);
+
+  const addElement = () => {
+    var newArray = [...initialElements , {id : idx, text:  (idx+1) }];
+    //newArray.push(<Items/>)
+    incr(idx + 1);
+    //console.log(initialElements.length);
+    setExampleState(newArray);
+    changeEl(newArray);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.list}>
+        <FlatList
+            keyExtractor = {item => item.id}  
+            data={exampleState}
+            renderItem = {item => (<Items><Text>{item.item.text}</Text><Button></Button></Items>)} />
+        </View>
+       <View style={styles.bottom}>
+       <Icon.Button
+            name="add-circle"
+            backgroundColor="#3b5998"        
+          title="Add element"
+          onPress={addElement}
+  >Add Item</Icon.Button>
+       </View>
     </View>
   );
 }
@@ -14,7 +47,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    borderWidth: 1
+  },
+  list:{
+    paddingTop:50,
+    
+  },
+  bottom:{
+    
+    alignItems:'center',
   },
 });
