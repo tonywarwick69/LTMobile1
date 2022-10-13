38151003c33bb847e16467531a0889adb758f737
tonywarwick69 <duc.khang165@gmail.com>
2022-10-13 09:48:25 +0700

    b7v1

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View style={styles.searchField}>
+          <Image style={styles.imageUser} source={require('./assets/Vector.png')}></Image>
+          <TextInput style={styles.inputText} placeholder="Put name to add"></TextInput> 
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text>Add</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text>Remove</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyle}>
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +30,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    
     justifyContent: 'center',
+  },
+  header:{
+    
+    marginTop:30,
+    flex:1,
+  },
+  searchField:{
+    flexDirection:'row',
+    backgroundColor:'lightgray',
+    border: '5px solid black',
+    height:70,
+    paddingLeft:50,
+    
+  },
+  imageUser:{
+   marginTop:25,
+  },
+  inputText:{
+   paddingLeft:20,
+  },
+  buttons:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:20,
+   
+  },
+  buttonStyle:{
+    backgroundColor:'#0033cc',
+    width:64,
+    height:40,
+    alignItems:'center',
+    fontSize:'large',
   },
 });
