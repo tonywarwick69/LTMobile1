657864ef56d835cf2f6d5e866ebe194b091f0103
tonywarwick69 <duc.khang165@gmail.com>
2022-11-03 08:56:25 +0700

    b9b1

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState  } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Button, Image } from 'react-native';
+import icon from './assets/Cat.jpg'
+
 
 export default function App() {
+
+  const [location, setLocation] 
+      = useState({
+        x: null,
+        y: null,
+        marginLeft: new Animated.Value(10),
+        marginTop: new Animated.Value(10)
+
+      });
+
+  function onPress(evt){
+       
+    
+    console.log('====================================');
+    var x = evt.nativeEvent.locationX;
+    console.log('====================================');
+    console.log(x);
+    console.log('====================================');
+    var y = evt.nativeEvent.locationY;
+    console.log('====================================');
+    setLocation({
+      x: x,
+      y: y,
+      marginLeft: x,
+      marginTop: y
+
+    })
+    
+    
+  }
+  function onMove(evt){
+    
+    
+    
+    console.log('====================================');
+    console.log(location);
+    console.log('====================================');
+    //setLocation({marginLeft: x, marginTop: y })
+  }
+  function onRelease(){
+    console.log("Realse!");
+  }
+  const {marginTop, marginLeft} = location;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      onStartShouldSetResponder={()=>true}
+      onMoveShouldSetResponder= {()=>true}
+      onResponderGrant={onPress}
+      onResponderMove={onMove}
+      onResponderRelease = {onRelease}
+      style={styles.container}>
+
+      <Text>Show something!</Text>
+     
+      <Animated.Image
+       source = {icon}
+       style={{marginLeft: marginLeft, marginTop: marginTop}}
+       >
+
+      </Animated.Image>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
+
   },
 });
